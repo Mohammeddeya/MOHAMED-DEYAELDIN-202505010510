fbc5f7ac2d8b9464bc4b413fb5922047001248fb
Mohammeddeya <dia0116674235@gmail.com>
2026-08-19 14:39:30 +0000

    thanks

---
+++ Week_2/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        //coding starts here;
+
+        Student s1 = new Student("Mohammed",21,4.0);
+        s1.displayInfo();
+        s1.study();
+        s1.takeExam();
+    }
+}
