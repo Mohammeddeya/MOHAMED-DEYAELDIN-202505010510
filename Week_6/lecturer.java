8efd1cc337ff283925d0684e8e11f9e315974f47
Mohammeddeya <dia0116674235@gmail.com>
2026-08-19 15:56:40 +0000

    Week_8_9

--- Week_6/lecturer.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        lecturer l = new lecturer("L101", "Dr Tarig","OOP", "Faculty of Information Technology");
-        l.displayInfo();
-        l.displaySubject();
-        l.displayDepartment();
-    }
-}
