ab69d90bbdcb93a3c27a830c736e457a6e86f837
Mohammeddeya <dia0116674235@gmail.com>
2026-08-19 16:00:47 +0000

    thanks

--- Week_3_4/Main.java
+++ Week_3_4/Main.java
@@ -1,4 +1,4 @@
-package tutorial_3;
+package Week_3_4;
 
 public class Main {
     public static void main(String[] args) {
--- Week_3_4/Student.java
+++ Week_3_4/Student.java
@@ -1,4 +1,4 @@
-package tutorial_3;
+package Week_3_4;
 
 public class Student extends Person {
 
